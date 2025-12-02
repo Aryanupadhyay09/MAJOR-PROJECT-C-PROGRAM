80e197021ed5cc06b5ab969bfcdaac1e3a5461a9
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-12-02 20:17:46 +0530

    Create KBC.h

---
+++ include/KBC.h
@@ -0,0 +1,10 @@
+#ifndef KBC_H
+#define KBC_H
+
+void welcome();
+void showQuestion(int qno);
+int checkAnswer(int qno, int userAnswer);
+void lifeline5050(int qno);
+void lifelineAudience(int qno);
+
+#endif
