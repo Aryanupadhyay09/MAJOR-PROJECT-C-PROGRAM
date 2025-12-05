31bcee49b68ddbf0f4221f07c6a01b817dcffcf2
Aryanupadhyay09 <aryanupa3696@gmail.com>
2025-12-05 13:20:27 +0530

    KBC.h

--- include/KBC.h
+++ include/KBC.h
@@ -2,9 +2,12 @@
 #define KBC_H
 
 void welcome();
+void showMainMenu();
 void showQuestion(int qno);
 int checkAnswer(int qno, int userAnswer);
 void lifeline5050(int qno);
-void lifelineAudience(int qno);
+void lifelineSkip();
+void showPrize(int level);
 
 #endif
+
